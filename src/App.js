476172dc19ed4633f643635d8d002b567cf40ad1
dev-lotus <lotushotmail111@gmail.com>
2021-08-20 23:01:26 +0530

    Search Page Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import SearchPage from "../src/screens/SearchPage/SearchPage";
+import { BrowserRouter, Route, Router } from "react-router-dom";
+import UserPage from "../src/screens/UserPage/UserPage";
+
 function App() {
   return (
-    <>
-      <SearchPage />
-    </>
+    <BrowserRouter>
+      <main>
+        <Route path="/" component={SearchPage} exact />
+        <Route path="/user" component={UserPage} exact />
+      </main>
+    </BrowserRouter>
   );
 }
 
